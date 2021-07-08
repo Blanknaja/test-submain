3a22af03e428c33d10057219a1a41e3591871bdb
DESKTOP-U0JP6LC\amzzz <athibet@home.co.th>
2021-07-09 00:42:17 +0700

    test add submodule

--- example-market/src/App.js
+++ example-market/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import  Hello from 'home-component-modules';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>TEST WEB MARKET</code> and save to reload.
         </p>
+        <Hello text="555555"/>
         <a
           className="App-link"
           href="https://reactjs.org"
