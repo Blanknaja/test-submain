a8715b271a20e3c3e369a626645ff1463ca2f14e
ATHIBET <athibet@home.co.th>
2021-07-11 00:42:24 +0700

    init git submodule

--- example-market/src/App.js
+++ example-market/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from "home-component-modules";
 function App() {
   return (
     <div className="App">
@@ -9,7 +8,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Hello text = "texttexttexttext" />
 
         <a
           className="App-link"
