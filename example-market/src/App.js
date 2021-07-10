14057454d08d50c7463860ab37a5aed588ec7087
ATHIBET <athibet@home.co.th>
2021-07-11 01:01:49 +0700

    no message

--- example-market/src/App.js
+++ example-market/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from 'home-component-modules';
 function App() {
   return (
     <div className="App">
@@ -8,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
+        <Hello text = "Hello"/>
         <a
           className="App-link"
           href="https://reactjs.org"
