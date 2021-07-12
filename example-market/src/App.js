d9212ce1ec1991c21503a640797f8f80aca529c0
ATHIBET <athibet@home.co.th>
2021-07-12 13:53:39 +0700

    update

--- example-market/src/App.js
+++ example-market/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 // import {TestComponent } from 'react-component-library';
-import { Hello , ProjectCardVertical } from 'home-component-modules';
+// import { Hello , ProjectCardVertical } from 'home-component-modules';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <TestComponent/> */}
-        <Hello text="Hello"/>
-        <ProjectCardVertical/>
+        {/* <Hello text="Hello"/> */}
+        {/* <ProjectCardVertical/> */}
       </header>
     </div>
   );
